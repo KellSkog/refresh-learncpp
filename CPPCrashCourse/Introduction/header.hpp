01845484444d4ffd6009e346df33a18fe8a4feba
Kell Skog <kell.skog@gmail.com>
2024-05-25 13:18:30 +0200

    Overture completed

--- CPPCrashCourse/Introduction/header.hpp
+++ CPPCrashCourse/Introduction/header.hpp
@@ -5,3 +5,23 @@ extern "C" {
         int goblin_count;
     };
 }
+
+struct Hal {
+    Hal(int version) : _version{version} {
+        std::cout << "I'm completely operational.\n";
+    }
+    ~Hal() {
+        std::cout << "Stop, Dave.\n";
+    }
+    void sing() {
+        std::cout << "Daisy Bell\n";
+    }
+    void sing(std::string song) {
+        std::cout << song << "\n";
+    }
+    static void zing(std::string song) {
+        std::cout << song << "\n";
+    }
+
+    const int _version;
+};
--- CPPCrashCourse/Introduction/main.cpp
+++ CPPCrashCourse/Introduction/main.cpp
@@ -1,15 +1,37 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+// #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.h"
 #include "header.hpp"
 #include <algorithm>
+#include <exception>
 #include <iostream>
+#include <memory> // smart pointers
+#include <system_error> // Throw
 #include <vector>
 
 void doItLikeCPP() {
     std::cout << "Printed by C++, called from C" << "\n";
 }
 
-    TEST_CASE("shuffle-the-array") {
+template <typename T>
+T add(T x, T y, T z) {
+    return x + y + z;
+}
+
+struct File {
+    File(const char* path, bool write) {
+        auto file_mode = write ? "w" : "r";
+        file_pointer = fopen(path, file_mode);
+        if (!file_pointer) throw std::system_error(errno, std::system_category());
+    }
+    ~File() {
+        fclose(file_pointer);
+    }
+    FILE* file_pointer;
+};
+
+
+TEST_CASE("shuffle-the-array") {
     SUBCASE("example1") {
         std::vector<int> x{ 0, 1, 8, 13, 5, 2, 3 };
         x[0] = 21;
@@ -28,5 +50,75 @@ void doItLikeCPP() {
         CHECK_EQ(x[5], 8);
         CHECK_EQ(x[6], 13);
         CHECK_EQ(x[7], 21);
-  }
+
+        auto n_evens = std::count_if(x.begin(), x.end(),
+            [] (auto number) { return number % 2 == 0; });
+        CHECK_EQ(n_evens, 2);
+
+        double d1=1.1, d2=2.2, d3= 3.3;
+        int i1=11, i2=22, i3=33;
+        CHECK_GT(add(d1,d2,d3), 6.599);
+        CHECK_LT(add(d1,d2,d3), 6.601);
+        CHECK_EQ(add(i1,i2,i3), 66);
+    }
+    SUBCASE("example2") {
+        Hal hal{9000};
+        Hal::zing("Zing a song\n");
+        hal.sing();
+        hal.sing("Sing a song\n");
+        // Listing 22
+        auto halal = new Hal(42);
+        CHECK_EQ(halal->_version, 42);
+        delete halal;
+    }
+    SUBCASE("Listing 24") {
+        try {
+            auto file{File("listing24.txt", true)};
+            const auto msg{"Hello world"};
+            fwrite(msg, strlen(msg), 1, file.file_pointer);
+        } catch (const std::exception &e) {
+            std::cout << "Unable to write file\n";
+        }
+        try {
+            auto file{File("listing24.txt", false)};
+            char read_msg[13];
+            fread(read_msg, sizeof(read_msg),1, file.file_pointer);
+            CHECK_EQ(strcmp(read_msg, "Hello world"), 0);
+            CHECK_LT(strcmp(read_msg, "Hello worlds"), 0);
+            CHECK_GT(strcmp(read_msg, "Gello world"), 0);
+        } catch (const std::exception &e) {
+            std::cout << "Unable to read file\n";
+        }
+
+    }
+    SUBCASE("Listing 26") {
+        struct Foundation{
+            Foundation(std::string founder) : _founder{founder} {}
+            ~Foundation(){
+                std::cout << "Foundation destroyed\n";
+            }
+            std::string _founder;
+        };
+        std::unique_ptr<Foundation> second_foundation{ new Foundation{"Wanda"} };
+        std::cout << second_foundation->_founder << "\n";
+    }
+    SUBCASE("Listing 27") {
+        struct Foundation{
+            Foundation(std::string founder) : _founder{founder} {}
+            std::string _founder;
+        };
+        struct Mutant {
+            // Constructor sets foundation appropriately:
+            Mutant(std::unique_ptr<Foundation> foundation)
+            : foundation(std::move(foundation)) {}
+            std::unique_ptr<Foundation> foundation;
+        };
+        std::unique_ptr<Foundation> second_foundation{ new Foundation{"Fish"} };
+        // ... use second_foundation
+        Mutant the_mule{ std::move(second_foundation) };
+        // second_foundation is in a 'moved-from' state
+        // the_mule owns the Foundation
+        std::cout << the_mule.foundation->_founder << "\n";
+        CHECK_EQ(second_foundation, nullptr);
+    }
 }
